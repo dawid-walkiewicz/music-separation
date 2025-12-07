Fix log power computation in STFT to remove unnecessary scaling

playground/stft.py
@@ -48,7 +48,7 @@ def compute_log_power_spectrogram(waveform, sr, n_fft=2048, hop_length=512, win_
     magnitude = stft.abs()
     power = magnitude ** 2
     # Przelicz na decybele (dB), zabezpiecz przed log(0)
-    log_power = 10.0 * torch.log10(power + eps)
+    log_power =  10 * torch.log10(power + eps)
     return log_power
 
 

training/new_model.py
@@ -9,7 +9,7 @@ def compute_log_power_spectrogram(waveform, n_fft=2048, hop_length=512, win_leng
     stft = torch.stft(waveform, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window, return_complex=True)
     magnitude = stft.abs()
     power = magnitude ** 2
-    log_power = 10.0 * torch.log10(power + eps)
+    log_power = torch.log10(power + eps)
     return log_power
 
 class NewSeparationModel(nn.Module):